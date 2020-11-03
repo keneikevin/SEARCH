ec804049af9c104b1935f8c7d177e6f294351207
kenei <kevinkenei9@gmail.com>
2020-11-03 17:35:44 +0300

    places api

--- app/src/main/java/com/example/search/MainActivity.kt
+++ app/src/main/java/com/example/search/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
+const val TAG:String="MAinActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        // Initialize the SDK
+        Places.initialize(applicationContext, getString(R.string.apikey))
+
+        // Create a new PlacesClient instance
+        val placesClient = Places.createClient(this)
+
+        // Create a new token for the autocomplete session. Pass this to FindAutocompletePredictionsRequest,
+        // and once again when the user makes a selection (for example when calling fetchPlace()).
+        val token = AutocompleteSessionToken.newInstance()
+
+        //Create a RectangularBounds object.
+        val bounds = RectangularBounds.newInstance(
+                LatLng(-33.880490, 151.184363),
+                LatLng(-33.858754, 151.229596)
+        )
+        //Use the builder to create a FindAutocompletePredictionsRequest.
+        val request = FindAutocompletePredictionsRequest.builder()
+       //Call either setLocationBias() / setLocationRestriction().
+                .setLocationBias(bounds)
+        //.setLocationrestriction(bounds)
+                .setOrigin(LatLng(-33.8749937, 151.2041382))
+                .setCountries("AU","NZ")
+                .setTypeFilter(TypeFilter.ADDRESS)
+                .setSessionToken(token)
+                .setQuery(query)
+                .build()
+        placesClient.findAutocompletePredictions(request)
+                .addOnSuccessListener { response: FindAutocompletePredictionsResponse ->
+                    for (prediction in response.autocompletePredictions) {
+                        Log.i(TAG, prediction.placeId)
+                        Log.i(TAG, prediction.getPrimaryText(null).toString())
+                    }
+    }.addOnFailureListener { exception: Exception? ->
+                    if (exception is ApiException) {
+                        Log.e(TAG, "Place not found: " + exception.statusCode)
+                    }
+                }}
 }
